5669b6c8f77af5abc36551d04561aa46222ce6f1
Johan-4P <3db9470n@students.codeinstitute.net>
2025-04-04 11:10:35 +0200

    Add product form for adding and editing products: implement category selection with friendly names and apply consistent styling to form fields

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """Form to add or edit a product."""
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+       
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        catetegories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in catetegories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+            
